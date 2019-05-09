e9693ec793f6c53f1785ebe6ab383f4e1878337e
Phuong <phuong.dang@pitech.asia>
2019-05-09 13:30:25 +0700

    update

--- WebSockets/ChatApplication/wwwroot/WebSocketManager.js
+++ WebSockets/ChatApplication/wwwroot/WebSocketManager.js
@@ -120,7 +120,7 @@ var WebSocketManager = (function () {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
                 var r = (d + Math.random() * 16) % 16 | 0;
                 d = Math.floor(d / 16);
-                return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+                return (c === 'x' ? r : r & 0x3 | 0x8).toString(16);
             });
         };
 
